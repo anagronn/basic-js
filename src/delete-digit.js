4a77d40d0d309659542741bc9084692a3ad0a9b7
anagronn <nastasya.vodchits@mail.ru>
2025-01-05 21:23:31 +0300

    feat:solve half

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const digits = String(n).split('').map((elem) => Number(elem));
+  const maxIndex = digits.indexOf(Math.max(...digits));
+  const minIndex = digits.indexOf(Math.min(...digits));
+  let digitsMinusOne = [];
+  if(maxIndex > 0){
+    digitsMinusOne = digits.filter((_, i) => i !== maxIndex - 1);
+  } else digitsMinusOne = digits.filter((_, i) => i !== minIndex);
+  return Number(digitsMinusOne.join(''));
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  const firstLetters = [];
+  if (Array.isArray(members)) {
+    members.forEach((member) => {
+      if (typeof member === 'string') firstLetters.push(member.trim()[0].toUpperCase());
+    })
+    return firstLetters.sort().join('');
+  }
+  return false;
 }
 
 module.exports = {
